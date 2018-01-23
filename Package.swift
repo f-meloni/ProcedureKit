71f1ab5405176540c4843feb3f07493d5e493d71
Franco Meloni <franco.meloni91@gmail.com>
2018-01-23 13:49:12 +0000

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -22,33 +22,7 @@ let package = Package(
 
         Target(
             name: "ProcedureKitNetwork",
-            dependencies: ["ProcedureKit"]),
-
-        /** Test executables */
-        Target(
-            name: "ProcedureKitTests",
-            dependencies: ["ProcedureKit", "TestingProcedureKit"]),
-
-         Target(
-            name: "ProcedureKitStressTests",
-            dependencies: ["ProcedureKit", "TestingProcedureKit"]),
-
-         Target(
-            name: "ProcedureKitCloudTests",
-            dependencies: ["ProcedureKitCloud", "TestingProcedureKit"]),
-
-         Target(
-            name: "ProcedureKitLocationTests",
-            dependencies: ["ProcedureKitLocation", "TestingProcedureKit"]),
-
-         Target(
-            name: "ProcedureKitMacTests",
-            dependencies: ["ProcedureKitMac", "TestingProcedureKit"]),
-
-         Target(
-            name: "ProcedureKitNetworkTests",
-            dependencies: ["ProcedureKitNetwork", "TestingProcedureKit"])
-
+            dependencies: ["ProcedureKit"])
     ],
 
     exclude: [
